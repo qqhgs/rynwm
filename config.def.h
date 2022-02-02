9c7a29c6dfec3918f4ab85e72a57128d2badc107
Ilham Bahtiar <bahtiari073@gmail.com>
2022-02-02 18:23:09 +0800

    change color

--- config.def.h
+++ config.def.h
@@ -7,11 +7,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono NFR:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#abb2bf";
+static const char col_gray4[]       = "#6d8dad";
+static const char col_cyan[]        = "#81A1C1";
+static const char col_bg2[]         = "#3b414d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
