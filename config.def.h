b0365ec29a1b636c09c9c18d807e814166fc482b
Ilham Bahtiar <bahtiari073@gmail.com>
2022-02-02 18:26:52 +0800

    add some keymaps

--- config.def.h
+++ config.def.h
@@ -28,8 +28,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "qutebrowser",        NULL,       NULL,       1 << 1,            0,         -1     },
+	{ "Firefox",            NULL,       NULL,       1 << 1,            0,         -1     },
+	{ "TelegramDesktop",    NULL,       NULL,       1 << 2,            0,         -1     },
 };
 
 /* layout(s) */
@@ -56,15 +57,26 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#include <X11/XF86keysym.h>
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *rofirun[]  = { "rofi", "-show", "drun", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = rofirun } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ 0,                            XF86XK_MonBrightnessUp,    spawn,          SHCMD("xbacklight -inc 5") },
+	{ 0,                            XF86XK_MonBrightnessDown,  spawn,          SHCMD("xbacklight -dec 5") },
+	{ 0,                            XF86XK_AudioMute,          spawn,          SHCMD("amixer -q set Master toggle") },
+	{ 0,                            XF86XK_AudioLowerVolume,   spawn,          SHCMD("amixer -q set Master 5%-") },
+	{ 0,                            XF86XK_AudioRaiseVolume,   spawn,          SHCMD("amixer -q set Master 5%+") },
+	{ 0,                            XK_Print,  spawn,          SHCMD("scrot /home/ryn/Pictures/screenshots/%Y-%m-%d-%H:%M:%S_$wx$h.png") },
+	{ MODKEY,                       XK_Print,  spawn,          SHCMD("scrot -u /home/ryn/Pictures/screenshots/%Y-%m-%d-%H:%M:%S_$wx$h.png") },
+	{ MODKEY|ShiftMask,             XK_Print,  spawn,          SHCMD("scrot -s /home/ryn/Pictures/screenshots/%Y-%m-%d-%H:%M:%S_$wx$h.png") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
